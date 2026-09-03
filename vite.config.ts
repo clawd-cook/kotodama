4a0f6ae3b81afd0bec4b0f0cbc127ea2e2f7bade
heyongqi10 <heyongqi10@jd.com>
2026-09-03 11:59:18 +0800

    config: add dependency exclusions in vite configuration to optimize build process

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: [
+      '**/.git/**',
+      '**/node_modules/**',
+      '**/submodules/**',
+      '**/.agents/**',
+    ]
+  },
   plugins: [react()],
   server: {
     watch: {
